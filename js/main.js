11264e11d2547a052a56cccaa65afad4e65d1a67
Emily Landgrebe <emilylandgrebe@emilys-mbp.lan>
2019-07-26 23:01:20 -0500

    Outlined html and css

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/ 
+// const players = {
+//     '0': 'white',
+//     '1': 'x',
+//     '2': 'o'
+// }
+
+/*----- app's state (variables) -----*/ 
+// let board, turn, winner;
+
+// var resetButton = document.getElementById('reset';
+
+/*----- cached element references -----*/ 
+
+
+/*----- event listeners -----*/ 
+
+
+/*----- functions -----*/
